3a6183cc9c7f76a42c8fa446e4e431a6c43d47ed
chicko98 <71787344+chicko98@users.noreply.github.com>
2022-12-24 21:23:34 +0000

    idk

---
+++ build/precache-manifest.0d56ccbd4630c9eedf6f2fae32712d1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a79c8d7f5493abb79592c10b310134",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6442f5e284204967f73a",
+    "url": "/static/css/main.7e9fdeb1.chunk.css"
+  },
+  {
+    "revision": "9e585b2dba47fcf6d552",
+    "url": "/static/js/2.3994da0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3994da0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6442f5e284204967f73a",
+    "url": "/static/js/main.a04f37db.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  }
+]);
